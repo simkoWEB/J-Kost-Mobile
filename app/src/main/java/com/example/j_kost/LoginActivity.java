032fd4bbfc66b55c01a098ecc6ab013bdbac6da2
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-10-21 17:23:46 +0700

    adding login page

---
+++ app/src/main/java/com/example/j_kost/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.j_kost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.loginBtn);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/j_kost/SplashScreen.java
+++ app/src/main/java/com/example/j_kost/SplashScreen.java
@@ -17,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 // Intent untuk pindah ke activity utama
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
