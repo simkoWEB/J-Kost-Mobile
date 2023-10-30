e9860752a503f2587b9e3e52789ee2aa7948ec7d
fachruddin <118855404+FachruddinFahma@users.noreply.github.com>
2023-10-30 13:04:55 +0700

    done interface (Home)

--- app/src/main/java/com/example/j_kost/HomeFragment.java
+++ app/src/main/java/com/example/j_kost/HomeFragment.java
@@ -3,10 +3,17 @@ package com.example.j_kost;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.j_kost.Adapter.HistoryAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +21,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private HistoryAdapter historyAdapter;
+    private List<String> listData;
+
+    // Fragment initialization parameters
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -56,9 +69,29 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Initialize RecyclerView
+        recyclerView = view.findViewById(R.id.kumpulan_card_history);
+        listData = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            listData.add("Transaksi bulan ke-" + i);
+        }
+
+        // Set up LinearLayoutManager and HistoryAdapter
+        linearLayoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        historyAdapter = new HistoryAdapter(requireContext(), listData);
+        recyclerView.setAdapter(historyAdapter);
+
+        // Notify adapter of data changes
+        historyAdapter.notifyDataSetChanged();
+
+        return view;
+
     }
 }
