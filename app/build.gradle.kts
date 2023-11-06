6701dcb3ffaf5901e540256057e31b26583b617a
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-11-07 02:02:57 +0700

    Tambah dependency dan Konfigurasi Network
    Konfigurasi network untuk penggunaan API dengan menambahkannya di xml dan mengubahnya di manifest.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,11 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.4.0-alpha01")
+
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
