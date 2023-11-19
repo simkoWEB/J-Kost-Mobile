48a6d41acf7e27cdbebd51437fb658f47421abd1
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-11-19 21:29:17 +0700

    add code spinner komplain

--- app/src/main/java/com/example/j_kost/Fragment/KomplainFragment.java
+++ app/src/main/java/com/example/j_kost/Fragment/KomplainFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -19,12 +20,49 @@ public class KomplainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_komplain, container, false);
 
-        Spinner jenis_komplain = view.findViewById(R.id.jenis_komplain);
+        final Spinner jenisKomplainSpinner = view.findViewById(R.id.jenis_komplain);
+        final Spinner masalahKomplainSpinner = view.findViewById(R.id.masalah_komplain);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.Komplain, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> jenisKomplainAdapter = ArrayAdapter.createFromResource(getContext(), R.array.TipeKomplain, android.R.layout.simple_spinner_item);
+        jenisKomplainAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        jenisKomplainSpinner.setAdapter(jenisKomplainAdapter);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        jenis_komplain.setAdapter(adapter);
+        jenisKomplainSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Mendapatkan pilihan yang dipilih pada spinner jenis komplain
+                String selectedTipeKomplain = parent.getItemAtPosition(position).toString();
+
+                // Set adapter untuk spinner masalah komplain berdasarkan pilihan pada jenis komplain
+                ArrayAdapter<CharSequence> masalahKomplainAdapter = null;
+                switch (selectedTipeKomplain) {
+                    case "Keamanan":
+                        masalahKomplainAdapter = ArrayAdapter.createFromResource(getContext(), R.array.komplainKeamanan, android.R.layout.simple_spinner_item);
+                        break;
+                    case "Fasilitas":
+                        masalahKomplainAdapter = ArrayAdapter.createFromResource(getContext(), R.array.komplainFasilitas, android.R.layout.simple_spinner_item);
+                        break;
+                    case "Kegaduhan":
+                        masalahKomplainAdapter = ArrayAdapter.createFromResource(getContext(), R.array.komplainKegaduhan, android.R.layout.simple_spinner_item);
+                        break;
+                    // Handle jika memilih "Pilih tipe komplain" atau "Lainnya"
+                    default:
+                        masalahKomplainAdapter = ArrayAdapter.createFromResource(getContext(), R.array.defaultArray, android.R.layout.simple_spinner_item);
+                        break;
+                }
+
+                // Set dropdown style
+                if (masalahKomplainAdapter != null) {
+                    masalahKomplainAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    masalahKomplainSpinner.setAdapter(masalahKomplainAdapter);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
         return view;
     }
 }
