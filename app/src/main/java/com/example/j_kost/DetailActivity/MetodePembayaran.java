a1a6bd3176f803485e9d72bcc099272b1488df21
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-11-22 13:22:36 +0700

    Menambahakan activity metode pembayaran

---
+++ app/src/main/java/com/example/j_kost/DetailActivity/MetodePembayaran.java
@@ -0,0 +1,69 @@
+package com.example.j_kost.DetailActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.example.j_kost.R;
+
+public class MetodePembayaran extends AppCompatActivity {
+
+    private ListView listViewMetodePembayaran;
+    private ArrayAdapter<String> adapter;
+    private SearchView searchView;
+    private String[] metodePembayaran = {
+            "Bank BRI",
+            "Bank BCA",
+            "Bank Jatim",
+            "Dana",
+            "Ovo",
+            "Shopeepay",
+            "Link Aja"
+            // ... tambahkan metode pembayaran lainnya di sini
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metode_pembayaran);
+
+        listViewMetodePembayaran = findViewById(R.id.listViewMetodePembayaran);
+        searchView = findViewById(R.id.searchView);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, metodePembayaran);
+        listViewMetodePembayaran.setAdapter(adapter);
+
+        listViewMetodePembayaran.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedValue = (String) adapterView.getItemAtPosition(i);
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("selectedValue", selectedValue);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+
+
+
+        // Mengatur fungsi pencarian
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+}
