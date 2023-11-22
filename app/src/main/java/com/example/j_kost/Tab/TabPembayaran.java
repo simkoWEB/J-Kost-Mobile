aa7ca40268ec920ddd55678fc995b39000d8457b
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-11-22 10:49:34 +0700

    memberikan format deciimal

--- app/src/main/java/com/example/j_kost/Tab/TabPembayaran.java
+++ app/src/main/java/com/example/j_kost/Tab/TabPembayaran.java
@@ -1,66 +1,93 @@
 package com.example.j_kost.Tab;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.j_kost.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TabPembayaran#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TabPembayaran extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText nominalBayar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TabPembayaran() {
-        // Required empty public constructor
+    public static String formatDec(int val) {
+        return String.format("%,d", val).replace('.', ',');
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TabPembayaran.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TabPembayaran newInstance(String param1, String param2) {
-        TabPembayaran fragment = new TabPembayaran();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    //untuk menghilangkan titik
+    public static String reFormat(String val) {
+        return val.replaceAll("[,.]", ""); // Menghapus semua koma dan titik
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_tab_pembayaran, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_pembayaran, container, false);
+        nominalBayar = view.findViewById(R.id.etNominal);
+        InputFilter filter = new InputFilter.LengthFilter(10);
+        nominalBayar.setFilters(new InputFilter[]{filter});
+
+        nominalBayar.addTextChangedListener(new TextWatcher() {
+            boolean isFormatting;
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Tambahkan validasi saat teks berubah
+                if (charSequence.length() > 10) {
+                    // Jika panjang input melebihi 8 karakter, maka batasi panjangnya
+                    String limitedInput = charSequence.toString().substring(0, 10);
+                    nominalBayar.setText(limitedInput); // Set ulang teks pada EditText
+                    nominalBayar.setSelection(limitedInput.length()); // Posisi kursor di akhir teks
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Cek apakah sedang dalam proses pemformatan untuk menghindari rekursi tak terbatas
+                if (isFormatting) return;
+
+                // Matikan pemformatan sementara agar tidak masuk ke dalam loop
+                isFormatting = true;
+
+                // Ambil teks yang dimasukkan
+                String originalText = editable.toString();
+
+                // Hapus tanda pemisah ribuan
+                String cleanString = originalText.replaceAll("[,.]", "");
+
+                // Cek apakah string kosong atau tidak
+                if (!cleanString.isEmpty()) {
+                    try {
+                        // Parsing ke tipe data long (bisa juga menggunakan double atau int, sesuai kebutuhan)
+                        long parsed = Long.parseLong(cleanString);
+
+                        // Format angka ke dalam format desimal yang diinginkan
+                        String formatted = formatDec((int) parsed);
+
+                        // Set hasil format ke dalam EditText
+                        nominalBayar.setText(formatted);
+                        nominalBayar.setSelection(formatted.length()); // Pindahkan kursor ke akhir teks
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                }
+                isFormatting = false;
+            }
+        });
+
+        return view;
     }
 }
