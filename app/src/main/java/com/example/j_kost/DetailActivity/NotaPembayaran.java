94d98c02f6f0cb3d55a44923a0237c7da55dee42
Debby Az-Zahra Firdauzi <78738330+hakimlutfi46@users.noreply.github.com>
2023-11-29 01:19:45 +0700

    add activity nota pembayaran

---
+++ app/src/main/java/com/example/j_kost/DetailActivity/NotaPembayaran.java
@@ -0,0 +1,16 @@
+package com.example.j_kost.DetailActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.j_kost.R;
+
+public class NotaPembayaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nota_pembayaran);
+    }
+}
