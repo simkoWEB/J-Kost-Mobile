792f0f68cceb21274bf6fd9a39416e5f76d1a73e
Debby Az-Zahra Firdauzi <78738330+hakimlutfi46@users.noreply.github.com>
2023-11-28 22:09:51 +0700

    Menambahkan API untuk cek password

--- app/src/main/java/com/example/j_kost/DetailActivity/ConfirmOldPass.java
+++ app/src/main/java/com/example/j_kost/DetailActivity/ConfirmOldPass.java
@@ -4,17 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.j_kost.R;
+import com.example.j_kost.Utils.MyToast;
+import com.example.j_kost.Utils.NetworkUtils;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConfirmOldPass extends AppCompatActivity {
     Button btnCofirm;
+    TextInputEditText oldPass;
     ImageView btnBack;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -24,19 +44,76 @@ public class ConfirmOldPass extends AppCompatActivity {
 
         btnCofirm = findViewById(R.id.confirmBtn);
         btnBack = findViewById(R.id.btnBack);
+        oldPass = findViewById(R.id.currentPass);
 
         btnCofirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(ConfirmOldPass.this, DetailGantiPassword.class);
-                startActivity(i);
+                String passwordToCheck = oldPass.getText().toString();
+
+                // Memeriksa apakah input password kosong atau tidak
+                if (passwordToCheck.isEmpty()) {
+                    // Jika kosong, tampilkan pesan kesalahan
+                    MyToast.showToastError(ConfirmOldPass.this, "Password tidak boleh kosong");
+                } else {
+                    // Jika tidak kosong, lanjutkan dengan proses permintaan HTTP POST menggunakan Volley
+                    SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("userData", Context.MODE_PRIVATE);
+                    String userId = sharedPreferences.getString("idUser", "");
+
+                    // Buat permintaan HTTP POST menggunakan Volley
+                    String url = "http://" + NetworkUtils.BASE_URL + "/PHP-MVC/public/EditDataApi/checkPassword/" + userId;
+
+                    // Persiapan untuk memulai permintaan
+                    RequestQueue queue = Volley.newRequestQueue(ConfirmOldPass.this);
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    try {
+                                        Log.d("Hasil Input", passwordToCheck);
+                                        Log.d("ConfirmOldPass", "Respons dari server: " + response); // Letakkan log di sini
+                                        JSONObject jsonObject = new JSONObject(response);
+                                        int code = jsonObject.getInt("code");
+                                        String status = jsonObject.getString("status");
+
+                                        if (code == 200) {
+                                            MyToast.showToastSuccess(ConfirmOldPass.this, "Password Benar");
+                                            Intent i = new Intent(ConfirmOldPass.this, DetailGantiPassword.class);
+                                            startActivity(i);
+                                        } else {
+                                            MyToast.showToastError(ConfirmOldPass.this, "Password Salah");
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    // Tangani kesalahan saat melakukan permintaan
+                                    error.printStackTrace();
+                                    MyToast.showToastError(ConfirmOldPass.this, "Terjadi kesalahan. Periksa koneksi internet Anda.");
+                                }
+                            }) {
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<>();
+                            params.put("password", passwordToCheck); // Sertakan kata sandi yang akan diverifikasi
+                            return params;
+                        }
+                    };
+
+                    // Tambahkan permintaan ke queue
+                    queue.add(stringRequest);
+                }
             }
         });
+
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                showCancelConfirmation();
                 finish();
             }
         });
@@ -46,51 +123,8 @@ public class ConfirmOldPass extends AppCompatActivity {
     @SuppressLint("MissingSuperCall")
     @Override
     public void onBackPressed() {
-//        showCancelConfirmation();
         finish();
     }
 
-    private void showCancelConfirmation() {
-        // Tampilkan alert dialog
-        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.DialogStyle);
-        builder.setTitle("Konfirmasi!");
-        builder.setMessage("Apakah Anda yakin ingin membatalkan perubahan?");
-
-        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                finish(); // Kembali ke layar sebelumnya
-            }
-        });
-
-        builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        AlertDialog alert = builder.create();
-        alert.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialog) {
-                // Mengubah teks tombol menjadi huruf kecil
-                Button positiveButton = ((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE);
-                if (positiveButton != null) {
-                    positiveButton.setAllCaps(false);
-                    positiveButton.setTextColor(getResources().getColor(android.R.color.holo_red_light)); // Mengubah warna teks
-                }
-
-                Button negativeButton = ((AlertDialog) dialog).getButton(DialogInterface.BUTTON_NEGATIVE);
-                if (negativeButton != null) {
-                    negativeButton.setAllCaps(false);
-                    negativeButton.setTextColor(getResources().getColor(R.color.black)); // Mengubah warna teks
-                }
-            }
-        });
-
-        alert.show();
-    }
 
 }
