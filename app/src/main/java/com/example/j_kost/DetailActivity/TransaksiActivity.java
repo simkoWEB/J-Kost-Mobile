acab6206cd74487a8f70cdfa07afa5fddf770223
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-12-05 10:36:25 +0700

    get img tapi from transaksi tapi belum bisa

--- app/src/main/java/com/example/j_kost/DetailActivity/TransaksiActivity.java
+++ app/src/main/java/com/example/j_kost/DetailActivity/TransaksiActivity.java
@@ -8,12 +8,15 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -41,6 +44,7 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
@@ -171,7 +175,7 @@ public class TransaksiActivity extends AppCompatActivity {
                     if (bayar < total) {
                         MyToast.showToastError(TransaksiActivity.this, "Nominal pembayaran kurang dari total yang harus dibayarkan");
                     } else {
-                        editTransaction(idTransaksi, String.valueOf(bayar), metodePembayaran);
+                        editTransaction(idTransaksi, String.valueOf(bayar), metodePembayaran, buktiPembayaranDrawable);
                     }
                 }
             }
@@ -207,7 +211,7 @@ public class TransaksiActivity extends AppCompatActivity {
         }
     }
 
-    private void editTransaction(String idTransaksi, String bayar, String metode) {
+    private void editTransaction(String idTransaksi, String bayar, String metode, Drawable photoBitmap) {
         String url = "http://" + NetworkUtils.BASE_URL + "/PHP-MVC/public/EditDataApi/editTransaction/" + idTransaksi;
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
@@ -252,6 +256,12 @@ public class TransaksiActivity extends AppCompatActivity {
                 Map<String, String> params = new HashMap<>();
                 params.put("bayar", bayar);
                 params.put("metode_pembayaran", metode);
+
+                // Encode gambar ke dalam format base64
+                String encodedImage = encodeImage(photoBitmap);
+                params.put("foto_bukti_bayar", encodedImage);
+
+
                 return params;
             }
         };
@@ -259,6 +269,16 @@ public class TransaksiActivity extends AppCompatActivity {
         requestQueue.add(stringRequest);
     }
 
+    private String encodeImage(Drawable drawable) {
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+        Bitmap bitmap = bitmapDrawable.getBitmap();
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+        return Base64.encodeToString(imageBytes, Base64.DEFAULT);
+    }
     private void getDataKost(String roomId) {
         String url = "http://" + NetworkUtils.BASE_URL + "/PHP-MVC/public/GetDataMobile/getUserKost/" + roomId;
 
